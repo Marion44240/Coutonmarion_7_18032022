cb878c3c3b813ed590c979cac3ed7cf200590ef8
Marion COUTON <shaines.jm@gmail.com>
2022-03-30 13:56:17 +0200

    creation page forum

---
+++ frontend/src/components/HeaderForum.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header-forum">
+    <img src="../assets/Logo-groupomania.png" alt="Logo Groupomania" />
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderForum",
+};
+</script>
+
+<style scoped lang="scss">
+header {
+  display: flex;
+  align-items: center;
+  height: 60px;
+  background: linear-gradient(violet, red);
+  padding: 10px;
+
+  img {
+    width: 150px;
+  }
+}
+</style>
---
+++ frontend/src/views/ForumView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="forum">
+    <HeaderForum/>
+    <main>
+      
+    </main>
+  </div>
+</template>
+
+<script>
+import HeaderForum from '@/components/HeaderForum.vue'
+
+export default {
+  name: 'ForumView',
+  components: {
+    HeaderForum
+  }
+}
+</script>
