16b63daa6e7bf2cd115e2fac4dbba37120ce9da4
Marion COUTON <shaines.jm@gmail.com>
2022-04-09 17:01:36 +0200

    debut frontend page forum

--- frontend/src/components/HeaderForum.vue
+++ frontend/src/components/HeaderForum.vue
@@ -1,18 +1,29 @@
 <template>
   <header class="header-forum">
     <img src="../assets/Logo-groupomania.png" alt="Logo Groupomania" />
+    <nav id="navbar">
+      <img @click="profil" src="../assets/avatar.png" alt="Boutton de profil">
+      <img @click="logout" src="../assets/logout.png" alt="Boutton de déconnexion">
+    </nav>
   </header>
 </template>
 
 <script>
 export default {
   name: "HeaderForum",
+  methods: {
+    logout() {
+      localStorage.clear();
+      this.$router.push('/');
+    }
+  },
 };
 </script>
 
 <style scoped lang="scss">
 header {
   display: flex;
+  justify-content: space-between;
   align-items: center;
   height: 60px;
   background: linear-gradient(violet, red);
@@ -20,6 +31,17 @@ header {
 
   img {
     width: 150px;
+  }
+}
+#navbar {
+  display:flex;
+  align-items: center;
+  height: 30px;
+  gap: 15px;
+  margin-right: 10px;
+  img {
+    width: 30px;
+    cursor: pointer;
   }
 }
 </style>
--- frontend/src/views/ForumView.vue
+++ frontend/src/views/ForumView.vue
@@ -2,7 +2,30 @@
   <div class="forum">
     <HeaderForum/>
     <main>
+      <section id="hello">
+        <h1>Bonjour {{ username }}</h1>
+      </section>
+
+      <section id="container">
+        <div id="container__forum">
+
+          <div id="container__publish">
+            <form>
+              <img src="../assets/avatar.png" alt="Photo de profil utilisateur">
+              <input type="text" placeholder="Créez une publication...">
+              <div id="button">
+                <button>Choisir une image</button>
+                <button>Publier !</button>
+              </div>         
+            </form>
+          </div>
+
+          <div id="container__post">
+            <h2>Fil d'actualité</h2>
+          </div>
+        </div>
       
+      </section>
     </main>
   </div>
 </template>
@@ -17,3 +40,62 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+main {
+  padding: 20px;
+  #hello {
+    display: flex;
+    text-align: center;
+    justify-content: center;
+    background: -webkit-linear-gradient(violet, red);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    font-size: 20px;
+  }
+  #container {
+    display: flex;
+    justify-content: center;
+
+    &__forum {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      box-shadow: 1px 1px 6px rgb(231, 75, 101);
+      border-radius: 8px;
+      width: 70%;   
+      padding: 15px;
+      margin: 20px 10px;
+      background: white;
+    }
+    &__publish {   
+      border-radius: 5px;
+      padding: 15px;
+      margin: 10px;
+      background: linear-gradient(violet, red);
+
+      form {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+        gap: 20px;
+        img {
+          width: 12%;
+        }
+        input {
+          width: 65%;
+          outline: none;
+          padding: 15px;
+        }
+        #button {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          width: 12%;
+          
+        }
+      }
+    }
+  }
+}
+</style>
